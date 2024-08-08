91319bacaac23d57b5adfd61007e84c8cf011dbd
Deepanjali Upadhyay <upadhyaydeepanjali4@gmail.com>
2024-08-08 23:49:57 +0530

    C++ Solution
            Submission Statistics:
            Question #: 2190
            Language: C++
            Runtime: 40 ms
            Runtime percentile: 18.46
            Memory: 22.616 MB
            Memory percentile: 62.92

---
+++ 2190. Count Common Words With One Occurrence/Solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countWords(vector<string>& words1, vector<string>& words2) {
+        map<string,int>m1,m2;
+        int count=0;
+
+        for(auto &a:words1){
+            ++m1[a];
+        }
+        for(auto &a:words2){
+            ++m2[a];
+        }
+
+        for(auto &a: words1){
+            if(m1[a]==1 && m2[a]==1){
+                count++;
+            }
+            
+        }
+
+        return count;
+    }
+};
