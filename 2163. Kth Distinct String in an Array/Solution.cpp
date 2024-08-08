55c3358aa1d9772907e6f262e5e26f97e57c2955
Deepanjali Upadhyay <upadhyaydeepanjali4@gmail.com>
2024-08-08 23:40:32 +0530

    C++ Solution
            Submission Statistics:
            Question #: 2163
            Language: C++
            Runtime: 22 ms
            Runtime percentile: 17.91
            Memory: 18.428 MB
            Memory percentile: 64.42

---
+++ 2163. Kth Distinct String in an Array/Solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string kthDistinct(vector<string>& arr, int k) {
+        map<string,int>m;
+
+        for(auto &a:arr){
+        m[a]++;
+        }
+
+        for(auto &a: arr){
+            if(m[a]==1)k--;
+            if(k==0) return a;
+        }
+        return "";
+    }
+};
