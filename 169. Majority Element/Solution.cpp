cb7643f8c13e3ff3c5f57561982506de0eca8239
Deepanjali Upadhyay <upadhyaydeepanjali4@gmail.com>
2024-08-15 01:24:16 +0530

    C++ Solution
            Submission Statistics:
            Question #: 169
            Language: C++
            Runtime: 19 ms
            Runtime percentile: 48.72
            Memory: 28.052 MB
            Memory percentile: 13.81

--- 169. Majority Element/Solution.cpp
+++ 169. Majority Element/Solution.cpp
@@ -4,7 +4,7 @@ public:
          
         int n=nums.size();
 
-        map<int,int>mpp;
+        unordered_map<int,int>mpp;
         for(int i=0;i<nums.size();i++){
             mpp[nums[i]]++;
     
@@ -19,6 +19,6 @@ public:
         
 
             
-       return -1; 
+       return 0; 
     }
 };
