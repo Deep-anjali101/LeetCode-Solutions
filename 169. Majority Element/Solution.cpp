7c695b92daafa999b50244003255bd8b09913d92
Deepanjali Upadhyay <upadhyaydeepanjali4@gmail.com>
2024-08-15 01:22:55 +0530

    C++ Solution
            Submission Statistics:
            Question #: 169
            Language: C++
            Runtime: 19 ms
            Runtime percentile: 48.72
            Memory: 27.024 MB
            Memory percentile: 43.56

---
+++ 169. Majority Element/Solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+         
+        int n=nums.size();
+
+        map<int,int>mpp;
+        for(int i=0;i<nums.size();i++){
+            mpp[nums[i]]++;
+    
+        }
+        
+
+        for(auto i: mpp){
+            if(i.second>n/2){
+                return i.first;
+            }
+        }
+        
+
+            
+       return -1; 
+    }
+};
