48118bdad1165e3ffd8fc013bee68a78796830f9
Deepanjali Upadhyay <upadhyaydeepanjali4@gmail.com>
2024-09-11 23:44:28 +0530

    C++ Solution
            Submission Statistics:
            Question #: 202
            Language: C++
            Runtime: 0 ms
            Runtime percentile: 100.00
            Memory: 7.392 MB
            Memory percentile: 89.75

---
+++ 202. Happy Number/Solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+int squareDigits(int n){
+        int sum=0;
+        while(n){
+            sum = sum + pow(n%10, 2);
+            n = n/10;
+        }
+        return sum;
+    }
+    bool isHappy(int n) {
+        while(n){ 
+            n = squareDigits(n);
+       
+        if(n==1)return true;
+        else if(n== 37){
+                return false;
+            }
+    
+        }
+        return false;
+       
+    }
+};
